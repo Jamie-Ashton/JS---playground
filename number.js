db0ca3a2a4b8be78896e9a4ed19bfb55138a3541
jamie-noel <jamie-ashton@live.nmit.ac.nz>
2024-03-07 14:58:25 +1300

    numbers

---
+++ number.js
@@ -0,0 +1,39 @@
+// variables 
+// const and let are better options,
+// const num1 = 10 // value cant changed.
+// let num2 = 20 // let is a better option by far.
+// var num3 = 30 //var declared inside the block are not global
+// w
+// console.log(num1)  
+// console.log(num2)
+// console.log(num3)
+
+// const persons = [
+//     {name: "jamie", age: 30},
+//     {name: "luke", age: 20}
+// ];
+
+
+
+// console.log(`My full name is ${persons[0].name} and ${persons[1].name}`)
+
+// const working = true;
+// const notworking = false;
+
+// if(working) {
+//     console.log("working")
+// }
+
+// if(age == 30) {
+//     console.log("working")
+// }
+
+let a = 0
+let result
+if(a > 0) {
+    result = "positive"
+    console.log(result)
+} else {
+    result = "negative"
+    console.log(result)
+}
